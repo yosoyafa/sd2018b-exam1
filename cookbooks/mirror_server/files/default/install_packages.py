483045b350a3905fcab6e7df50a3aa94d27850fc
yosoyafa <carlosafanador97@gmail.com>
2018-10-04 17:37:51 -0500

    Update

--- cookbooks/mirror_server/files/default/install_packages.py
+++
@@ -1,7 +0,0 @@
-with open(/home/vagrant/packages.json) as f:
-    package = json.load(f)
-
-#for i in leer:
-#        x = i["commands"].split(";")
-#	for l in x:
-#os.system("yum install --downloadonly --downloaddir=/var/repo " + l)
