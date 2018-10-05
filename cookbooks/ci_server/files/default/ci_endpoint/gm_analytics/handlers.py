3e0bec151cefbf05ce5344a199ef91eee3b9d762
yosoyafa <carlosafanador97@gmail.com>
2018-10-05 18:06:04 -0500

    Update files

--- cookbooks/ci_server/files/default/ci_endpoint/gm_analytics/handlers.py
+++ cookbooks/ci_server/files/default/ci_endpoint/gm_analytics/handlers.py
@@ -5,21 +5,16 @@ from fabric import Connection
 from flask import request
 
 def manage_pullrequest():
-    update = request.get_data().decode('utf8')
-    updateJSON = json.load(update)
-    pr = jsonFile["pull_request"]["head"]["sha"]
+    update = request.get_data()
+    updateUTF = str(update, 'utf-8')
+    updateJSON = json.loads(updateUTF)
+    pr = updateJSON["pull_request"]["head"]["sha"]
     url = 'https://raw.githubusercontent.com/yosoyafa/sd2018b-exam1/' + pr + '/packages.json'
-    content = request.get(url)
-    packages_json = json.loads(response.content)
-
-    for a in content:
-
-        package = a
-        ic = package[installation_commands]
-
-        for command in ic:
-
-            Connection('vagrant@192.168.131.103').run('sudo yum install --downloadonly --downloaddir=/var/repo' + command)
-
+    contents = requests.get(url)
+    pck = json.loads(contents.content)
+    for package in pck:
+        cmd = package['installation_commands']
+        cmdstr = ''.join(cmd)
+        Connection('vagrant@192.168.131.103').run('sudo yum install --downloadonly --downloaddir=/var/repo'+ ' ' + cmdstr)
     out = {'command_return': '1'}
-return out
+    return out
