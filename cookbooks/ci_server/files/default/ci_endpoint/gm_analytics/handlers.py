e212cb4638d808a0bc76465d1e8ff7710ea7596a
yosoyafa <carlosafanador97@gmail.com>
2018-10-06 16:29:25 -0500

    Update files

--- cookbooks/ci_server/files/default/ci_endpoint/gm_analytics/handlers.py
+++ cookbooks/ci_server/files/default/ci_endpoint/gm_analytics/handlers.py
@@ -1,3 +1,9 @@
+import os
+import requests
+import json
+from fabric import Connection
+from flask import request
+
 def manage_pullrequest():
     update = request.get_data()
     updateUTF = str(update, 'utf-8')
@@ -6,7 +12,7 @@ def manage_pullrequest():
     url = 'https://raw.githubusercontent.com/yosoyafa/sd2018b-exam1/' + pr + '/packages.json'
     contents = requests.get(url)
     pck = json.loads(contents.content)
-    for package in pck:	
+    for package in pck:
         cmd = package['installation_commands']
         cmdstr = ''.join(cmd)
         Connection('vagrant@192.168.131.103').run('sudo yum install --downloadonly --downloaddir=/var/repo'+ ' ' + cmdstr)
